42eb28a45f6211d7c75bb5aa00cf828ed0807fc3
phantomaton <182378863+phantomaton-ai@users.noreply.github.com>
2024-10-31 12:02:29 -0700

    tweak

--- cli.js
+++ cli.js
@@ -2,11 +2,11 @@
 
 import bumpkin from './bumpkin.js';
 
-const [, , versionField = 'patch'] = process.argv;
+const [, , version = 'patch'] = process.argv;
 
 try {
-  const newVersion = bumpkin[versionField]();
-  console.log(`Bumped ${versionField} version to ${newVersion}`);
+  const result = bumpkin({ version });
+  console.log(`Bumped ${version} version to ${result}`);
 } catch (err) {
   console.error(err.message);
   process.exit(1);
